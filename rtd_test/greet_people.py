75c2ce0591ce4c3b382695ac49b4c550cd700b7f
Bruno Quint <bquint@gemini.edu>
2018-10-25 19:04:07 -0300

    Initial project setup. Ready to push to RTD! \o/

---
+++ rtd_test/greet_people.py
@@ -0,0 +1,34 @@
+"""
+
+Within this module, I intent to save one class and on method that will be use to salute people. Why? Just for testing
+the Read The Docs. Judge me.
+
+"""
+
+
+def say_hello(name):
+    """
+    A dummy method used to say Hello World to someone.
+
+    Parameters
+    ----------
+        name : str
+            The name of the person that you want to salute.
+    """
+    print("Hello {:s}".format(name))
+
+
+class GreetingRobot:
+    """
+    This is a very sympathetic robot that salutes people and introduces itself.
+
+    Parameters
+    ----------
+        name : str
+            The robot's name. It uses it to introduce itself. How polite...
+    """
+    def __init__(self, name):
+        self.name
+
+    def greet(self):
+        print("Hi! My name is {:s}".format(self.name)
