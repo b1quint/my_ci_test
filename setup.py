54094f20ad324eb8e5d3f7b133fb6eaaf9ba7f5d
Bruno Quint <bquint@gemini.edu>
2018-11-13 10:54:14 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!python
+
+import setuptools
+
+setuptools.setup(
+    name='my_ci_test',
+    version='0.0.1',
+    author='Bruno Quint',
+    author_email='bquint@gemini.edu',
+    description='A package created to test RTD and CI',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independentent",
+    ],
+)
